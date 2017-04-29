6a988fb5b925f651aa90a97f4de621cfb08316b3
chenxin <chenxin6676@163.com>
2017-04-30 00:33:23 +0800

    add 0429 file

--- 0429.py
+++ 0429.py
@@ -39,4 +39,18 @@ def prod(x, y):
     return x*y
 print(reduce(prod, [1, 2, 3, 4]))
 
+print('########以下是filter函数用法##########')
+
+
+def is_odd(n):
+    return n % 2 == 1
+print(list(filter(is_odd, [1, 2, 3, 4, 5, 6, 7])))
+
+
+def not_empty(s):
+    return s and s.strip()
+print(list(filter(not_empty, ['A', '', None, 'C', ' '])))
+
+
+
 
