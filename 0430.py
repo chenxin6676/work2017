b571c4483236d1857a72083104b3fd258cbd9bbd
chenxin <chenxin6676@163.com>
2017-05-01 21:43:36 +0800

    add 0429 file

--- 0430.py
+++ 0430.py
@@ -1,29 +1,22 @@
 __author__ = 'chenxin'
 
 
-def _odd_iter():
-    n = 1
-    while True:
-        n = n + 2
-        yield n
+print(sorted([-1, -2, 3, 2, 9, 7]))
+print(sorted([-1, -2, 3, 2, 9, 7], key=abs))
+print(sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower))
+print(sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower, reverse=True))
+
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
 
 
-def _not_divisible(n):
-    return lambda x: x % n > 0
+def by_name(t):
+    return t[0]
+print(sorted(L, key=by_name))
 
 
-def primes():
-    yield 2
-    it = _odd_iter()
-    while True:
-        n = next(it)
-        yield n
-        it = filter(_not_divisible(n), it)
+def by_score(t):
+    return t[-1]
+print(sorted(L, key=by_score, reverse=True))
 
-
-for n in primes():
-    if n < 1000:
-        print(n)
-    else:
-        break
-
+print(sorted(L, key=lambda x: x[0]))
+print(sorted(L, key=lambda x: x[1], reverse=True))
