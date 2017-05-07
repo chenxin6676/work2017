689039e3fd45c50949e69cd93220a357eef452b9
chenxin <chenxin6676@163.com>
2017-05-07 23:45:15 +0800

    date update

--- 0507.py
+++ 0507.py
@@ -1,35 +1,49 @@
 __author__ = 'Chenxin'
 
-# 使用map高阶函数求列表各个元素的平方
-from collections import Iterator
+# # 使用map高阶函数求列表各个元素的平方
+# from collections import Iterator
+#
+#
+# def func(x):
+#     return x**2
+# lst = [1, 2, 3, 4, 5]
+# itor = map(func, lst)
+# print(itor)
+# print(type(itor))
+# print(isinstance(itor, Iterator))
+# for i in itor:
+#     print(i)
+#
+#
+# print([x for x in map(str, [1, 2, 3, 4])])
+# print(list(map(str, [5, 6, 7, 8, 9, 0])))
+#
+# # 使用reduce计算列表和
+# from functools import reduce
+# lst2 = [1, 2, 3]
+#
+#
+# def add2(x, y):
+#     return x + y
+#
+# reds = reduce(add2, lst2)
+# print(type(reds))
+# print(reds)
+#
+# # 把列表转换成单个数字
+# def fn(x, y):
+#     return x*10+y
+# print(reduce(fn, [1, 3, 5, 7, 9]))
 
-
-def func(x):
-    return x**2
-lst = [1, 2, 3, 4, 5]
-itor = map(func, lst)
-print(type(itor))
-print(isinstance(itor, Iterator))
-for i in itor:
-    print(i)
-
-
-print([x for x in map(str, [1, 2, 3, 4])])
-print(list(map(str, [5, 6, 7, 8, 9, 0])))
-
-# 使用reduce计算列表和
+# 将给出的数字字符串列表显示为串在一起的一个整数(按照列表默认顺序).
 from functools import reduce
-lst2 = [1, 2, 3]
 
 
-def add2(x, y):
-    return x + y
+def char2num(s):
+    return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
 
-reds = reduce(add2, lst2)
-print(type(reds))
-print(reds)
 
-# 把列表转换成单个数字
-def fn(x, y):
-    return x*10+y
-print(reduce(fn, [1, 3, 5, 7, 9]))
+def str2int(s):
+    return reduce(lambda x, y: x*10+y, map(char2num, s))
+
+print(str2int(['1', '3', '9']))
