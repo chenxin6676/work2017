f1337bab3e4154049aa0f2b4c428c9bb16747872
chenxin <chenxin6676@163.com>
2017-05-08 15:38:25 +0800

    update from chenxin study

--- 0502.py
+++ 0502.py
@@ -6,12 +6,13 @@ def count():
     for i in range(1, 4):
         def f():  # 这里的定义函数没有带参数,可以跟下面定义的f函数对比
             return i*i
-#        fs.append(f)
-        fs.append(f())
+        fs.append(f)
+#        fs.append(f())
     return fs
 print(count())
-# for j in count():
-#     print(j())
+for j in count():
+    print(j())
+print('################')
 
 
 def count():
