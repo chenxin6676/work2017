73905234c964b68471c40431c223baf5951f9601
chenxin <chenxin6676@163.com>
2017-05-09 19:22:25 +0800

    update from chenxin study

--- 0509.py
+++ 0509.py
@@ -1,25 +1,94 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# #!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+#
+# """ a test module """
+#
+#
+# __author__ = 'Chenxin'
+#
+# import sys
+#
+#
+# def fn():
+#     args = sys.argv
+#     if len(args) == 1:
+#         print('Hello one args: %s' % args[0])
+#     elif len(args) == 2:
+#         print('Hello two args: %s' % args[1])
+#     else:
+#         print('Sorry, Too many arguments!')
+#
+#
+# if __name__ == '__main__':
+#     fn()
 
-""" a test module """
+# #!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+#
+# """ this is a test modle"""
+#
+# __author__ = 'Chenxin'
+#
+#
+# import sys
+#
+#
+# def test():
+#     args = sys.argv
+#     if len(args) == 1:
+#         print('Your arguments is one %s' % args)
+#     elif len(args) == 2:
+#         print('Your arguments is two %s' % args)
+#     else:
+#         print('Your arguments Too many!')
+#
 
 
-__author__ = 'Chenxin'
+def __privilege_1(name):
+    print('Your name is 1: %s' % name)
 
-import sys
+
+def __privilege_2(name):
+    print('Your name is 2: %s' % name)
 
 
-def fn():
-    args = sys.argv
-    if len(args) == 1:
-        print('Hello one args: %s' % args[0])
-    elif len(args) == 2:
-        print('Hello two args: %s' % args[1])
-    else:
-        print('Sorry, Too many arguments!')
+def namep(name):
+    __privilege_1(name)
+    __privilege_2(name)
 
 
 if __name__ == '__main__':
-    fn()
+    # test()
+    namep('Chenxin')
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
