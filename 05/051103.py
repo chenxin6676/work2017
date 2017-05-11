1c2da2e64296f5f0853116fdfb2f958fd423a3dd
chenxin <chenxin6676@163.com>
2017-05-12 00:16:38 +0800

    date update

--- 05/051103.py
+++ 05/051103.py
@@ -5,22 +5,22 @@ from types import MethodType
 对实例的绑定方法只有1种可以.就是通过MethodType
 '''
 
+
 def set_age(self, age):
     self.age = age
-
 
 
 class Stu(object):
     pass
 # Stu.set_age=MethodType(set_age,Stu)
-# Stu.set_age = set_age
+Stu.set_age = set_age
 A = Stu()
-# B = Stu()
+B = Stu()
 # A.set_age=MethodType(set_age,A)
 # B.set_age=MethodType(set_age,B)
-A.set_age = set_age
+# A.set_age = set_age
 # B.set_age = set_age
 A.set_age(10)
-# B.set_age(15)
-# print(A.age, B.age)
-print(A.age)
+B.set_age(15)
+print(A.age, B.age)
+# print(A.age)
