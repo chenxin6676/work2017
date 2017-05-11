027730022ff0f1fb3d3b33534a4ec859654adf78
chenxin <chenxin6676@163.com>
2017-05-11 22:52:12 +0800

    date update

--- 05/051102.py
+++ 05/051102.py
@@ -1,1 +1,30 @@
-__author__ = 'chenxin'
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Chenxin'
+'''
+这里介绍的是实例绑定变量或绑定方法中,__slots__的用法
+'''
+
+class Student(object):
+    __slots__ = ('name', 'age', 'register')
+    pass
+
+
+s = Student()
+s.name = 'Chenxin2'
+s.age = 30
+print(s.name, s.age)
+
+
+def register(self, name):
+    self.name = name
+    print(self.name)
+
+
+from types import MethodType
+
+
+s.register = MethodType(register, s)
+
+s.register(20)
