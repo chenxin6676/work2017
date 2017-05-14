a2f60768912afe67eeb6b9121e321447c8da0607
chenxin <chenxin6676@163.com>
2017-05-14 22:14:17 +0800

    date update

--- 05/051401.py
+++ 05/051401.py
@@ -1,1 +1,19 @@
-__author__ = 'chenxin'
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Chenxin'
+
+
+class Student(object):
+    def __init__(self, name):
+        self.__name = name
+
+    def __str__(self):
+        return 'Your name is %s, Hello!' % self.__name
+    __repr__ = __str__
+
+
+
+s = Student('Mechile')
+print(s)
+Student('Zhangsan')
---
+++ 05/051403.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Chenxin'
+
+
+class Fib(object):
+    def __getitem__(self, item):
+        a, b = 1, 1
+        for x in range(item):
+            a, b = b, a+b
+        return a
+
+f = Fib()
+print(f[5])
