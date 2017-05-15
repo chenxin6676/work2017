ad9ce1406b9b258f4ffcbc4b6e0edbd668423dc1
chenxin <chenxin6676@163.com>
2017-05-15 19:23:37 +0800

    update from chenxin study

--- 05/051503.py
+++ 05/051503.py
@@ -13,3 +13,12 @@ class Student(object):
 
 s = Student('Michle')
 s()
+
+
+from enum import Enum
+
+Month = Enum('Month2', 'Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec')
+
+for name, member in Month.__members__.items():
+    print(name, '=>', member, ',', member.value)
+
