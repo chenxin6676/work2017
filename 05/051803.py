e937660b017cd5cca6a92006b33c0f8c043a2229
chenxin <chenxin6676@163.com>
2017-05-18 14:29:08 +0800

    update from chenxin study

--- 05/051803.py
+++ 05/051803.py
@@ -4,14 +4,21 @@
 # IO的操作
 # f = open('/Users/chenxin/gitsvn/localwork/05/0518.txt', 'r')
 
+print('11111111')
 try:
     f = open('/Users/chenxin/gitsvn/localwork/05/0518.txt', 'r')
     print(f.read())
 finally:
     if f:
         f.close()
-
-
+print()
+print('222222222')
 with open('/Users/chenxin/gitsvn/localwork/05/0518.txt', 'r') as f:
     print(f.read())
+print()
+print('333333333')
+with open('/Users/chenxin/gitsvn/localwork/05/0518.txt', 'r') as file:
+    line = file.readlines()
+    for oneline in line:
+        print(oneline.strip())
 
