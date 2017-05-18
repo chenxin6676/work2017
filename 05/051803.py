05d2e9f96fbd0317ef0eb861c986b9914f2c4940
chenxin <chenxin6676@163.com>
2017-05-18 14:47:29 +0800

    update from chenxin study

--- 05/051803.py
+++ 05/051803.py
@@ -4,21 +4,23 @@
 # IO的操作
 # f = open('/Users/chenxin/gitsvn/localwork/05/0518.txt', 'r')
 
-print('11111111')
-try:
-    f = open('/Users/chenxin/gitsvn/localwork/05/0518.txt', 'r')
-    print(f.read())
-finally:
-    if f:
-        f.close()
-print()
-print('222222222')
-with open('/Users/chenxin/gitsvn/localwork/05/0518.txt', 'r') as f:
-    print(f.read())
-print()
-print('333333333')
-with open('/Users/chenxin/gitsvn/localwork/05/0518.txt', 'r') as file:
-    line = file.readlines()
-    for oneline in line:
-        print(oneline.strip())
 
+# try:
+#     f = open('/Users/chenxin/gitsvn/localwork/05/0518.txt', 'r')
+#     print(f.read())
+# finally:
+#     if f:
+#         f.close()
+# with open('/Users/chenxin/gitsvn/localwork/05/0518.txt', 'r') as f:
+#     print(f.read())
+# with open('/Users/chenxin/gitsvn/localwork/05/0518.txt', 'r') as file:
+#     line = file.readlines()
+#     for oneline in line:
+#         print(oneline.strip())
+with open('/Users/chenxin/gitsvn/localwork/05/0518.txt', 'a') as writefile:
+    writefile.write('6 line\n')
+
+with open('/Users/chenxin/gitsvn/localwork/05/0518.txt', 'r') as readfile:
+    eveline = readfile.readlines()
+    for line in eveline:
+        print(line.strip())
