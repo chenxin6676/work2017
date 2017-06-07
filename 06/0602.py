23ca764eca24e54213572b23d16520d638d8ce2c
chenxin <chenxin6676@163.com>
2017-06-07 16:54:22 +0800

    update from chenxin study

--- 06/0602.py
+++ 06/0602.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+from multiprocessing import  Process, Queue
+import os, time, random
+
+
+def write(q):
+    print('Process %s write' % os.getpid())
+    for value in ['A', 'B', 'C']:
+        print('Put %s to queue...' % value)
+        q.put(value)
+        time.sleep(random.random())
+
+
+def read(q):
+    print('Process %s read' % os.getpid())
+    while True:
+        value = q.get(True)
+        print('Get %s from queue' % value)
+
+
+if __name__ == '__main__':
+    q = Queue()
+    pw = Process(target=write, args=(q,))
+    pr = Process(target=read, args=(q,))
+    pw.start()
+    pr.start()
+    pw.join()
+    pr.terminate()
 
